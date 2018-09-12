7893b1f23f50b4b61a69c11ae13961ca63fb1ac1
lwatson98 <petrie.watson@gmail.com>
2018-09-11 20:31:47 -0400

    Leighton Watson Lab 1 205

--- app/routes.py
+++ app/routes.py
@@ -12,12 +12,28 @@ def index():
 def artists():
 
     artists =[
-        {'author': {'artists': 'Tears'},
+        {'band': {'name': 'Tears'},
             'body': ''
         },
         {
-            'author': {'artists': 'The Blasts'},
+            'band': {'name': 'The Blasts'},
             'body': ''
         }
     ]
     return render_template('artists.html', title='Home', artists=artists);
+@app.route('/Tears')
+def Tears():
+
+    info =[
+        {'body': 'Tears is an Upstate NY, hard rockin, boogie band assembled by bassist, Jeff Howell, formerly of Foghat, The Outlaws and Savoy Brown, to name a few.'}
+    ]
+
+    return render_template('Tears.html', title="Home", info=info);
+@app.route('/new_artist')
+def Newartists():
+
+    warning =[
+        {'body': 'Under Construction'}
+    ]
+
+    return render_template('new_artist.html', title="Home", warning=warning);
